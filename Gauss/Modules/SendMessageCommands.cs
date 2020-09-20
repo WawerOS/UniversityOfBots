85a06c8921754362df653f0c918c5775150d9c3b
StringEpsilon <StringEpsilon@gmail.com>
2020-09-20 19:02:00 +0200

    Added MPL 2.0 license.

--- Gauss/Modules/SendMessageCommands.cs
+++ Gauss/Modules/SendMessageCommands.cs
@@ -1,13 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.IO;
-using System.Linq;
+/**
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+**/
+
 using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
-using DSharpPlus.Entities;
-
 
 namespace Gauss.Commands {
     public class SendMessageCommands: BaseCommandModule {
--- Gauss/Program.cs
+++ Gauss/Program.cs
@@ -1,3 +1,9 @@
+/**
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+**/
+
 using System;
 using System.IO;
 using Gauss.Models;
@@ -10,6 +16,7 @@ namespace Gauss
     {
         static void Main(string[] args){
             string configDirectory = Path.Join(GetFolderPath(SpecialFolder.UserProfile), "GaussBot");
+
             if (args.Length > 0 && args[0] == "--configDir"){
                 configDirectory = args[1];
             }
@@ -20,6 +27,7 @@ namespace Gauss
                 throw new Exception($"'config.json' was not found in '{configDirectory}'.");
             }
             GaussConfig config = JsonUtility.Deserialize<GaussConfig>(Path.Join(configDirectory, "config.json"));
+
 
             var bot = new GaussBot(config);
             bot.Connect();
