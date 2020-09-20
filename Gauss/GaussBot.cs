0f3ef8692a407d8809fb3c114292e0997c64aff8
StringEpsilon <StringEpsilon@gmail.com>
2020-09-20 22:53:32 +0200

    Make bot use modules and commands implemented so far.

--- Gauss/GaussBot.cs
+++ Gauss/GaussBot.cs
@@ -8,33 +8,41 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
+using Gauss.Commands;
 using Gauss.Models;
+using Gauss.Modules;
 
 namespace Gauss {
-    public class GaussBot{
-        private readonly DiscordClient _client;
+	public class GaussBot {
+		private readonly DiscordClient _client;
+		private readonly GaussConfig _config;
+		private readonly List<object> _modules = new List<object>();
 
-		public GaussBot(GaussConfig config){
-            this._client = new DiscordClient(new DiscordConfiguration{
-                Token = config.DiscordToken,
-            });
+		public GaussBot(GaussConfig config) {
+			this._config = config;
+			this._client = new DiscordClient(new DiscordConfiguration {
+				Token = config.DiscordToken,
+			});
 
-            var commandConfig = new CommandsNextConfiguration {
-				StringPrefixes = new List<string>() { config.CommandPrefix },
+			var commandConfig = new CommandsNextConfiguration {
+				StringPrefixes = new List<string>() { _config.CommandPrefix },
 				EnableDms = true,
 				EnableMentionPrefix = true,
 			};
-
 			this._client.UseCommandsNext(commandConfig);
-        }
+			this._client.GetCommandsNext().RegisterCommands<SendMessageCommands>();
+			
+			
+			this._modules.Add(new RoleAssign(this._client, _config));
+		}
 
-        public void Connect(){
-            Task.Run(() => this._client.ConnectAsync());
-        }
+		public void Connect() {
+			Task.Run(() => this._client.ConnectAsync());
+		}
 
-        public void Disconnect() {
-            this._client.DisconnectAsync().GetAwaiter().GetResult();
-            this._client.Dispose();
-        }
-    }
+		public void Disconnect() {
+			this._client.DisconnectAsync().GetAwaiter().GetResult();
+			this._client.Dispose();
+		}
+	}
 }
